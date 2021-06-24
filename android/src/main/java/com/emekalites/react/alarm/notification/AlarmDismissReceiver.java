4cf162bda6484a77ec2c3b66d5295e09d38feab6
Ahmad <achmadfaz@gmail.com>
2021-06-24 07:58:51 +0700

    fix alarm dismiss

--- android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
+++ android/src/main/java/com/emekalites/react/alarm/notification/AlarmDismissReceiver.java
@@ -12,14 +12,12 @@ public class AlarmDismissReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         AlarmUtil alarmUtil = new AlarmUtil((Application) context.getApplicationContext());
         try {
-            if (ANModule.getReactAppContext() != null) {
-                int notificationId = intent.getExtras().getInt(Constants.DISMISSED_NOTIFICATION_ID);
-                ANModule.getReactAppContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("OnNotificationDismissed", "{\"id\": \"" + notificationId + "\"}");
+            int notificationId = intent.getExtras().getInt(Constants.DISMISSED_NOTIFICATION_ID);
+            ANModule.getReactAppContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("OnNotificationDismissed", "{\"id\": \"" + notificationId + "\"}");
 
-                alarmUtil.removeFiredNotification(notificationId);
+            alarmUtil.removeFiredNotification(notificationId);
 
-                alarmUtil.doCancelAlarm(notificationId);
-            }
+            alarmUtil.doCancelAlarm(notificationId);
         } catch (Exception e) {
             alarmUtil.stopAlarmSound();
             System.err.println("Exception when handling notification dismiss. " + e);
